21b7b6af3c6889388701a69021521adfdc2a5163
Felipe Oliveira <fdeoliveira@quaresitsolutions.com>
2023-06-23 15:48:51 -0300

    md iOS logic to hide alert

--- ios/RnDynamicAppIcon.swift
+++ ios/RnDynamicAppIcon.swift
@@ -23,6 +23,22 @@ class DynamicAppIcon: NSObject {
                 UIApplication.shared.setAlternateIconName(nil)
                 return
             }
-            UIApplication.shared.setAlternateIconName(iconName)
+            setApplicationIconName(iconName)
+        }
+
+    @objc
+        func setApplicationIconName(_ iconName: String) {
+            if UIApplication.shared.responds(to: #selector(getter: UIApplication.supportsAlternateIcons)) && UIApplication.shared.supportsAlternateIcons {
+
+                typealias setAlternateIconName = @convention(c) (NSObject, Selector, NSString, @escaping (NSError) -> ()) -> ()
+
+                let selectorString = "_setAlternateIconName:completionHandler:"
+
+                let selector = NSSelectorFromString(selectorString)
+                let imp = UIApplication.shared.method(for: selector)
+                let method = unsafeBitCast(imp, to: setAlternateIconName.self)
+                method(UIApplication.shared, selector, iconName as NSString, { _ in })
+            }
         }
 }
+
